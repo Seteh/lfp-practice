0d7c00d66571509985cefd48327396699b6f7786
Igor Kriklivetc <igor.kriklivets@hotmail.com>
2015-12-10 23:05:46 +0300

    the second lab, part #1

--- fpLabs/fpLabs/Labs/lab2.js
+++ fpLabs/fpLabs/Labs/lab2.js
@@ -2,12 +2,33 @@
 
 // возвращает True если аргумент содержит повторяющиеся элементы.
 function duplicates(array) {
-    // здесь должен быть ваш код
+    var impl = function(i, array) {
+        if(i >= array.length)
+            return false;
+
+        if(array.lastIndexOf(array[i]) > i)
+            return true;
+
+        return impl(i + 1, array);
+    };
+
+    return impl(0, array);
 }
 
 // возвращает массив уникальных элементов из переданного массива.
 function removeDuplicates(array) {
-    // здесь должен быть ваш код
+    var impl = function(array, i, accumulator) {
+        if(i >= array.length)
+            return accumulator;
+
+        if(accumulator.indexOf(array[i]) > -1)
+            return impl(array, i + 1, accumulator);
+
+        accumulator.push(array[i]);
+        return impl(array, i + 1, accumulator);
+    };
+
+    return impl(array, 0, []);
 }
 
 test("Повторяющиеся элементы", function() {
@@ -19,6 +40,20 @@ test("Повторяющиеся элементы", function() {
 
 // Пересечение двух массивов A и B — это массив только с теми элементами A и B, которые одновременно принадлежат обоим массивам, без дублей.
 function intersection(array1, array2) {
+    var impl = function(left, right, i, accumulator) {
+        if(i == left.length)
+            return accumulator;
+
+        if(accumulator.indexOf(left[i]) > -1)
+            return impl(left, right, i + 1, accumulator);
+
+        if(right.indexOf(left[i]) > -1)
+            accumulator.push(left[i]);
+
+        return impl(left, right, i + 1, accumulator);
+    };
+
+    return impl(array2, array1, 0, impl(array1, array2, 0, []));
 }
 
 test("Пересечение массивов", function() {
@@ -27,6 +62,17 @@ test("Пересечение массивов", function() {
 
 // Разность двух массивов A и B — это массив с элементами A, не совпадающими с элементами B, без дублей.
 function arr_diff(array1, array2) {
+    var impl = function(left, right, i, accumulator) {
+        if(i == left.length)
+            return accumulator;
+
+        if(accumulator.indexOf(left[i]) < 0 && right.indexOf(left[i]) < 0)
+            accumulator.push(left[i]);
+
+        return impl(left, right, i + 1, accumulator);
+    };
+
+    return impl(array1, array2, 0, []);
 }
 
 test("Разность массивов", function() {
@@ -36,6 +82,17 @@ test("Разность массивов", function() {
 
 // Объединение двух массивов A и B — это массив с элементами A и элементы массива B, без дублей.
 function arr_sum(array1, array2) {
+    var impl = function(array, i, accumulator) {
+        if(i === array.length)
+            return accumulator;
+
+        if(accumulator.indexOf(array[i]) < 0)
+            accumulator.push(array[i]);
+
+        return impl(array, i + 1, accumulator);
+    };
+
+    return impl(array2, 0, impl(array1, 0, []));
 }
 
 test("Объединение массивов", function() {
@@ -45,6 +102,7 @@ test("Объединение массивов", function() {
 
 // Симметрическая разность двух массивов A и B — это такой массив, куда входят все те элементы первого массива, которые не входят во второй массив, а также те элементы второго массива, которые не входят в первый массив.
 function symmetricDiff(array1, array2) {
+    // здесь будет код
 }
 
 test("Симметрическая разность массивов", function() {
